4d5b61864eb57ac1918f7be6f91965e74df3499b
Abiyoso Wahyu Rahardjo Putro <61867360+abiyosowr@users.noreply.github.com>
2021-02-26 08:43:32 +0700

    Menggenerate file CSV to TfRecord

---
+++ generate_tfrecord.py
@@ -0,0 +1,88 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import io
+import pandas as pd
+import tensorflow.compat.v1 as tf
+import sys
+sys.path.append("C:\\Users\\Abiyosowr\\Tensorflow\\models-1.13.0\\research") #sesuaikan folder temen2
+sys.path.append("C:\\Users\\Abiyosowr\\Tensorflow\\models-1.13.0\\research\\object_detection") #sesuaikan folder temen2
+from PIL import Image
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+flags = tf.app.flags
+flags.DEFINE_string('type', '', 'Type of CSV input (train/test)')
+flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
+flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+FLAGS = flags.FLAGS
+def class_text_to_int(row_label): #sesuaikan banyak klasifikasi temen-temen
+    if row_label == 'Dua Puluh Ribu':
+        return 1
+    elif row_label=='Dua Ribu':
+        return 2
+    elif row_label=='Lima Puluh Ribu':
+        return 3
+    elif row_label=='Lima Ribu':
+        return 4
+    elif row_label=='Sepuluh Ribu':
+        return 5
+    elif row_label=='Seratus Ribu':
+        return 6
+    elif row_label=='Seribu':
+        return 7
+    else:
+        return 0
+def split(df, group):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+def main(_):
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    path = os.path.join(os.getcwd(), 'images/{}'.format(FLAGS.type))
+    examples = pd.read_csv(FLAGS.csv_input)
+    grouped = split(examples, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, path)
+        writer.write(tf_example.SerializeToString())
+    writer.close()
+    output_path = os.path.join(os.getcwd(), FLAGS.output_path)
+    print('Successfully created the TFRecords: {}'.format(output_path))
+if __name__ == '__main__':
+    tf.app.run()
